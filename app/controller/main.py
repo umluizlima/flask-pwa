ca7e0240119686d955dc8b9c87cacaa13403ffe7
umluizlima <umluizlima@gmail.com>
2018-05-18 12:24:41 -0300

    Add to Home Screen banner can now be triggered by a button on the page.

--- app/controller/main.py
+++ app/controller/main.py
@@ -8,4 +8,4 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('main/index.html',
-                           title='Flask')
+                           title='Flask-PWA')
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -13,3 +13,31 @@
     });
   }
 })();
+
+let deferredPrompt;
+const btnAdd = document.querySelector('#btnAdd');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log('beforeinstallprompt event fired');
+  e.preventDefault();
+  deferredPrompt = e;
+  btnAdd.style.display = 'block';
+});
+
+btnAdd.addEventListener('click', (e) => {
+  btnAdd.style.display = 'none';
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+
+window.addEventListener('appinstalled', (evt) => {
+  app.logEvent('app', 'installed');
+});
