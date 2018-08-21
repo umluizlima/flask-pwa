63cf6ce2c2ab7855477174770bf2923c7c9dc739
umluizlima <umluizlima@gmail.com>
2018-08-21 12:42:19 -0300

    Commented out mkdir of instance

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,21 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True, static_url_path='')
+    app = Flask(
+        __name__,
+        instance_relative_config=True,
+        static_url_path=''
+    )
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY') or 'you-will-never-guess',
         # SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(app.instance_path, 'app.db'),
         # SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # try:
+    #     os.makedirs(app.instance_path)
+    # except OSError:
+    #     pass
 
     from flask_sslify import SSLify
     if 'DYNO' in os.environ:  # only trigger SSLify if the app is running on Heroku
