ff48a703ca04b5b34ae46daa7dbeed24807f1b76
Seb <duerr.sebastian@gmail.com>
2022-08-23 21:19:05 -0700

    rm pyscript

--- app/controller/main.py
+++ app/controller/main.py
@@ -9,3 +9,14 @@ bp = Blueprint('main', __name__)
 def index():
     return render_template('main/index.html',
                            title='Joy & Seb will marry')
+
+
+@bp.route('/get_paraphrase', methods=['post'])
+def get_paraphrase():
+    try:
+        str_response = [1,2,3]
+        return app.response_class(response=json.dumps(str_response), status=200, mimetype='application/json')
+    except Exception as error:
+        err = str(error)
+        print(err)
+        return app.response_class(response=json.dumps(err), status=500, mimetype='application/json')
